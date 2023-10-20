99c5cb81f4f1c704c203114b78dd32aa33593477
Daniel Arndt <daniel.arndt@canonical.com>
2023-10-20 09:07:39 -0300

    Set units to blocked status when scaling beyond 1 (#24)

--- src/charm.py
+++ src/charm.py
@@ -54,7 +54,13 @@ class SMFOperatorCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         if not self.unit.is_leader():
-            raise NotImplementedError("Scaling is not implemented for this charm")
+            # NOTE: In cases where leader status is lost before the charm is
+            # finished processing all teardown events, this prevents teardown
+            # event code from running. Luckily, for this charm, none of the
+            # teardown code is necessary to preform if we're removing the
+            # charm.
+            self.unit.status = BlockedStatus("Scaling is not implemented for this charm")
+            return
         self._container_name = self._service_name = "smf"
         self._container = self.unit.get_container(self._container_name)
         self._nrf_requires = NRFRequires(charm=self, relation_name="fiveg_nrf")
